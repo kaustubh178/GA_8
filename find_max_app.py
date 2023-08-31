626f489fb86a1b34b4b79359091d3b9625c80e97
kaustubh178 <143702964+kaustubh178@users.noreply.github.com>
2023-08-31 15:34:36 +0530

    Update find_max_app.py

--- find_max_app.py
+++ find_max_app.py
@@ -6,14 +6,14 @@ Type "help", "copyright", "credits" or "license()" for more information.
 ...     return max(a, b, c)
 ... 
 ... def main():
-...     st.title("Find the Largest Number")
+...     st.title("Find the largest Number")
 ... 
-...     st.write("Enter three numbers:")
+...     st.write("Please Enter three numbers:")
 ...     a = st.number_input("Enter the first number:")
 ...     b = st.number_input("Enter the second number:")
 ...     c = st.number_input("Enter the third number:")
 ... 
-...     if st.button("Find Largest"):
+...     if st.button("Find the Largest"):
 ...         largest = find_largest(a, b, c)
 ...         st.success(f"The largest number is: {largest}")
 ... 
