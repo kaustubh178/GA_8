6238770328285653bbfce0276cfce8d294e76291
kaustubh178 <143702964+kaustubh178@users.noreply.github.com>
2023-08-31 16:06:05 +0530

    Update find_max_app.py

--- find_max_app.py
+++ find_max_app.py
@@ -1,19 +1,20 @@
 import streamlit as st
-... 
-... def find_largest(a, b, c):
-...     return max(a, b, c)
-... 
-... def main():
-...     st.title("Find the largest Number")
-... 
-...     st.write("Please Enter three numbers:")
-...     a = st.number_input("Enter the first number:")
-...     b = st.number_input("Enter the second number:")
-...     c = st.number_input("Enter the third number:")
-... 
-...     if st.button("Find the Largest"):
-...         largest = find_largest(a, b, c)
-...         st.success(f"The largest number is: {largest}")
-... 
-... if __name__ == "__main__":
-...     main()
+
+def find_largest(a, b, c):
+    return max(a, b, c)
+
+def main():
+    st.title("Find the Largest Number")
+
+    st.write("Enter three numbers:")
+    a = st.number_input("Enter the first number:")
+    b = st.number_input("Enter the second number:")
+    c = st.number_input("Enter the third number:")
+
+    if st.button("Find Largest"):
+        largest = find_largest(a, b, c)
+        st.success(f"The largest number is: {largest}")
+
+if __name__ == "__main__":
+    main()
+
